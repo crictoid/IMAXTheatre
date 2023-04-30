d7a2869006be3a890d840f4f6c290aedaa4607e2
Mark David Bulmer <mdbulmer86@gmail.com>
2023-04-30 18:17:34 -0400

    Add files via upload

---
+++ BulmerCh7CPA1/frmIMAX.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmIMAX
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.cboShowing = New System.Windows.Forms.ComboBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnTicketCost = New System.Windows.Forms.Button()
+        Me.txtTickets = New System.Windows.Forms.TextBox()
+        Me.lblTickets = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.lblTitle.Font = New System.Drawing.Font("Myanmar Text", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(44, 10)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(314, 51)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "IMAX Theatre Tickets"
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'cboShowing
+        '
+        Me.cboShowing.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboShowing.FormattingEnabled = True
+        Me.cboShowing.Items.AddRange(New Object() {"Matinee $16", "Evening $27"})
+        Me.cboShowing.Location = New System.Drawing.Point(127, 83)
+        Me.cboShowing.Name = "cboShowing"
+        Me.cboShowing.Size = New System.Drawing.Size(149, 32)
+        Me.cboShowing.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.lblCost)
+        Me.Panel1.Controls.Add(Me.btnClear)
+        Me.Panel1.Controls.Add(Me.btnTicketCost)
+        Me.Panel1.Controls.Add(Me.txtTickets)
+        Me.Panel1.Controls.Add(Me.lblTickets)
+        Me.Panel1.Controls.Add(Me.lblTitle)
+        Me.Panel1.Controls.Add(Me.cboShowing)
+        Me.Panel1.Location = New System.Drawing.Point(396, -1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(403, 464)
+        Me.Panel1.TabIndex = 4
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.lblCost.Font = New System.Drawing.Font("Myanmar Text", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCost.ForeColor = System.Drawing.Color.White
+        Me.lblCost.Location = New System.Drawing.Point(96, 361)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(211, 43)
+        Me.lblCost.TabIndex = 9
+        Me.lblCost.Text = "XXXXXXXXXXXX"
+        Me.lblCost.Visible = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Font = New System.Drawing.Font("Myanmar Text", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.White
+        Me.btnClear.Location = New System.Drawing.Point(114, 273)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(174, 56)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear Form"
+        Me.btnClear.UseVisualStyleBackColor = False
+        Me.btnClear.Visible = False
+        '
+        'btnTicketCost
+        '
+        Me.btnTicketCost.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnTicketCost.Font = New System.Drawing.Font("Myanmar Text", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTicketCost.ForeColor = System.Drawing.Color.White
+        Me.btnTicketCost.Location = New System.Drawing.Point(114, 202)
+        Me.btnTicketCost.Name = "btnTicketCost"
+        Me.btnTicketCost.Size = New System.Drawing.Size(174, 56)
+        Me.btnTicketCost.TabIndex = 6
+        Me.btnTicketCost.Text = "Ticket Cost"
+        Me.btnTicketCost.UseVisualStyleBackColor = False
+        Me.btnTicketCost.Visible = False
+        '
+        'txtTickets
+        '
+        Me.txtTickets.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTickets.Location = New System.Drawing.Point(170, 165)
+        Me.txtTickets.Name = "txtTickets"
+        Me.txtTickets.Size = New System.Drawing.Size(63, 31)
+        Me.txtTickets.TabIndex = 4
+        Me.txtTickets.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtTickets.Visible = False
+        '
+        'lblTickets
+        '
+        Me.lblTickets.AutoSize = True
+        Me.lblTickets.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTickets.Location = New System.Drawing.Point(105, 118)
+        Me.lblTickets.Name = "lblTickets"
+        Me.lblTickets.Size = New System.Drawing.Size(192, 25)
+        Me.lblTickets.TabIndex = 3
+        Me.lblTickets.Text = "Number of Tickets:"
+        Me.lblTickets.Visible = False
+        '
+        'frmIMAX
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.BulmerCh7CPA1.My.Resources.Resources.imax
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "frmIMAX"
+        Me.Text = "Purchase IMAX Theatre Tickets"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents cboShowing As ComboBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblCost As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnTicketCost As Button
+    Friend WithEvents txtTickets As TextBox
+    Friend WithEvents lblTickets As Label
+End Class
---
+++ BulmerCh7CPA1/frmIMAX.vb
@@ -0,0 +1,106 @@
+﻿' Program Name:		IMAX Theatre
+' Author:			Mark Bulmer 
+' Date:				March 28, 2022
+' Purpose:			The IMAX Theatre app determines the showing and calculates the total cost of a specified number of tickets. 
+
+Option Strict On
+
+Public Class frmIMAX
+
+	Private Sub cbolocation_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboShowing.SelectedIndexChanged
+		' This event handler allows the user to enter the showing choice
+		Dim intShowingChoice As Integer
+
+		intShowingChoice = cboShowing.SelectedIndex
+
+		' Make items visible in the window 
+		lblTickets.Visible = True
+		txtTickets.Visible = True
+		btnTicketCost.Visible = True
+		btnClear.Visible = True
+		lblCost.Visible = True
+		' Clear the labels
+		lblCost.Text = ""
+		' Set focus on number in tickets text box
+		txtTickets.Focus()
+	End Sub
+	Private Sub btnTicketCost_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTicketCost.Click
+		' This button event handler determines the number of tickets and 
+		' displays the cost
+
+
+		Dim intTicketNumber As Integer
+		Dim blnNumberOfTicketsIsValid As Boolean = False
+		Dim intShowingChoice As Integer
+		Dim decTotalCost As Decimal
+
+		' Call a function to ensure the number of tickets is valid 
+		blnNumberOfTicketsIsValid = ValidateNumberOfTickets()
+		' If number of tickets is valid, calculate the cost 
+		If (blnNumberOfTicketsIsValid) Then
+			intTicketNumber = Convert.ToInt32(txtTickets.Text)
+			intShowingChoice = cboShowing.SelectedIndex
+			Select Case intShowingChoice
+				Case 0
+					decTotalCost = intTicketNumber * 16
+				Case 1
+					decTotalCost = intTicketNumber * 27
+			End Select
+			' Display the cost of the scuba dive 
+			lblCost.Text = decTotalCost.ToString("C") & " for the tickets"
+		End If
+	End Sub
+
+	Private Function ValidateNumberOfTickets() As Boolean
+		' This procedure validates the value entered for the number of tickets selected
+		Dim intTicketNumber As Integer
+		Dim blnValidityCheck As Boolean = False
+		Dim strNumberOfTicketsMessage As String = "Please enter the number of tickets (1-12)"
+		Dim strMessageBoxTitle As String = "Error"
+
+		Try
+			intTicketNumber = Convert.ToInt32(txtTickets.Text)
+			If intTicketNumber >= 1 And intTicketNumber <= 12 Then
+				blnValidityCheck = True
+			Else
+				MsgBox(strNumberOfTicketsMessage, , strMessageBoxTitle)
+				txtTickets.Focus()
+				txtTickets.Clear()
+			End If
+		Catch Exception As FormatException
+			MsgBox(strNumberOfTicketsMessage, , strMessageBoxTitle)
+			txtTickets.Focus()
+			txtTickets.Clear()
+		Catch Exception As OverflowException
+			MsgBox(strNumberOfTicketsMessage, , strMessageBoxTitle)
+			txtTickets.Focus()
+			txtTickets.Clear()
+		Catch Exception As SystemException
+			MsgBox(strNumberOfTicketsMessage, , strMessageBoxTitle)
+			txtTickets.Focus()
+			txtTickets.Clear()
+		End Try
+		Return blnValidityCheck
+	End Function
+
+	Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+		' This event handler clears the form and resets the form for reuse when the user clicks the Clear button. 
+		cboShowing.Text = "Select Location"
+		txtTickets.Clear()
+		lblCost.Text = ""
+		lblTickets.Visible = False
+		txtTickets.Visible = False
+		btnTicketCost.Visible = False
+		btnClear.Visible = False
+		lblCost.Visible = False
+	End Sub
+
+	Private Sub frmIMAX_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		' Hold the splash screen for 4 seconds 
+		Threading.Thread.Sleep(4000)
+	End Sub
+
+	Private Sub lblTitle_Click(sender As Object, e As EventArgs) Handles lblTitle.Click
+
+	End Sub
+End Class
